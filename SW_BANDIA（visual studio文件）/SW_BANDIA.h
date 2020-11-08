6f0e10ed707bd5d9d6641abca80f59428bb24194
Caesarhhh <45118905+Caesarhhh@users.noreply.github.com>
2020-11-08 14:46:37 +0800

    初次上传

---
+++ SW_BANDIA（visual studio文件）/SW_BANDIA.h
@@ -0,0 +1,32 @@
+﻿
+// SW_BANDIA.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CSWBANDIAApp:
+// 有关此类的实现，请参阅 SW_BANDIA.cpp
+//
+
+class CSWBANDIAApp : public CWinApp
+{
+public:
+	CSWBANDIAApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSWBANDIAApp theApp;
